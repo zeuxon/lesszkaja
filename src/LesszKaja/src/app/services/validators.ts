790dc9892cd333f73e47dfa98e8f0c480b25c50c
Kelemen Bálint Péter <h365052@stud.u-szeged.hu>
2024-11-05 21:03:35 +0100

    Mezők ellenőrzése regisztrációnál, ellenőrző funkciók létrehozása

---
+++ src/LesszKaja/src/app/services/validators.ts
@@ -0,0 +1,58 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+// Csak whitespace-e az input?
+export function noWhitespaceMinValidator(minLength:number): ValidatorFn {
+  return (control: AbstractControl) => {
+    const isWhitespace = ((control.value || '').trim().length === 0)
+    const isWhitespaceLongEnough=(control.value || '').trim().length >= minLength;
+    return (!isWhitespace && isWhitespaceLongEnough) ? null : { whitespace: true };
+  };
+}
+
+// Jó email-e?
+export function strictEmailValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const valid = emailRegex.test(control.value);
+
+    return valid ? null : { strictEmail: true };
+  };
+}
+
+// Jó telefonszám?
+export function phoneValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const phoneRegex = /^[0-9]{11}$/;
+    const valid = phoneRegex.test(control.value);
+
+    return valid ? null : { phone: true };
+  };
+}
+
+export function textValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const textRegex = /^\S+$/;
+    const valid = textRegex.test(control.value);
+
+    return valid ? null : { text: true };
+  };
+}
+
+export function nameValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const nameRegex = /^([a-zA-ZéÉíÍüÜőŐáÁúÚóÓ]){3,}([" "]([a-zA-ZéÉíÍüÜőŐáÁúÚóÓ]){3,})*$/;
+    const valid = nameRegex.test(control.value);
+
+    return valid ? null : { name: true };
+  };
+}
+
+export function passwordValidator(): ValidatorFn {
+  return (control: AbstractControl) => {
+    const passwordRegex = /^\S{5,}$/;
+    const valid = passwordRegex.test(control.value);
+
+    return valid ? null : { password: true };
+  };
+}
+
--- src/LesszKaja/src/app/users/visitor/register/register.component.ts
+++ src/LesszKaja/src/app/users/visitor/register/register.component.ts
@@ -1,22 +1,37 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule} from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { hashSync } from "bcrypt-ts";
+import * as validator from "../../../services/validators";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, CommonModule, HttpClientModule],
+  imports: [FormsModule, CommonModule, HttpClientModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 
 export class RegisterComponent {
 
-    constructor(private http: HttpClient) {}
+  userForm : FormGroup;
+  isFormSubmitted: boolean = false;
+
+    constructor(private http: HttpClient) {
+        this.userForm = new FormGroup({
+          felhasznalonev: new FormControl("",[Validators.required,validator.nameValidator()]),
+          email: new FormControl("",[Validators.required,validator.strictEmailValidator()]),
+          tel: new FormControl("",[Validators.required,validator.phoneValidator()]),
+          lakcim: new FormControl("",[Validators.required,validator.textValidator()]),
+          jelszo: new FormControl("",[Validators.required,validator.passwordValidator()])
+        })
+    }
 
     onSubmit(form: any) {
+      const isFormValid = this.userForm.valid;
+      this.isFormSubmitted=true;
+
       const userData = {
         felhasznalonev: form.value.username,
         emailcim: form.value.email,
