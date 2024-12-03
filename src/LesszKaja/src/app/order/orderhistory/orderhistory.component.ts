63e01fb0d018928e1346403e957b92591abe788c
Gál Gergő Károly <h260678@stud.u-szeged.hu>
2024-12-03 21:44:30 +0100

    rendelés előzmények componens létrehozása

---
+++ src/LesszKaja/src/app/order/orderhistory/orderhistory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orderhistory',
+  standalone: true,
+  imports: [],
+  templateUrl: './orderhistory.component.html',
+  styleUrl: './orderhistory.component.scss'
+})
+export class OrderhistoryComponent {
+
+}
