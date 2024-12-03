92e698c230b9b999c66fa96dce6861376a0ca188
Kelemen Bálint Péter <h365052@stud.u-szeged.hu>
2024-12-03 16:28:08 +0100

    Restaurantlist megjavítása

--- src/LesszKaja/src/app/restaurantslist/list/list.component.ts
+++ src/LesszKaja/src/app/restaurantslist/list/list.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  @Input() show: Array<{ id: number; nev: string; image: string; eteltipusok?: { nev: string }[] }> = [];
+  @Input() show: Array<{ id: number; nev: string; image: string; eteltipusok?: { nev: string }[], route: string}> = [];
 
   ngOnInit() {
     this.show.forEach(item => {
